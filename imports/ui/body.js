48cd368eba7c8cf3db247b93b73de3b83730c3ce
Dillon Coffman <dillonscoffman@gmail.com>
2017-08-15 17:30:54 -0400

    Added ability to add tasks via forms & events into DB, added ability to remove tasks

--- imports/ui/body.js
+++ imports/ui/body.js
@@ -1,11 +1,32 @@
 import { Template } from 'meteor/templating';
 
-import { Tasks } from '../api/tasks.js'
+import { Tasks } from '../api/tasks.js';
 
-import './body.html'
+import './task.js';
+import './body.html';
 
 Template.body.helpers({
   tasks() {
-    return Tasks.find({});
+    return Tasks.find({}, { sort: { createdAt: -1 } });
   },
+});
+
+Template.body.events({
+  'submit .new-task'(event){
+    //prevents default browser form submit
+    event.preventDefault();
+
+    //get value from form element
+    const target = event.target;
+    const text = target.text.value;
+
+    //Insert a task into collection
+    Tasks.insert({
+      text,
+      createdAt: new Date(), //the current time
+    });
+
+    //clear form
+    target.text.value = '';
+  }
 });
